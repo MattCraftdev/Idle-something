cd72f415ed80eb6e99346c58838faa82266568ed
MattCraftdev <you@example.com>
2025-07-25 00:24:12 -0400

    Pre=alpha V1.4
    V1.2 - Switched to OOP and cleaned up code
    V1.3 - Added level counters, and increasing difficulty
    V1.4 - Fixed a visual bug with some math

--- main.js
+++ main.js
@@ -1,50 +1,79 @@
-// Varibles
-let progress1 = 0;
-let progress2 = 0;
-let str = 0;
-let activebar = null;
 
-const jobProgress1 = document.getElementById("jobProgress1");
-const jobProgress2 = document.getElementById("jobProgress2");
+class ProgressBar {
+    constructor(elementId, speed = 1, maxprogress = 100) {
+        this.progress = 0;
+        this.maxprogress = maxprogress;
+        this.level = 1;
+        this.speed = speed;
+        this.element = document.getElementById(elementId);
+    }
 
-function updateProgress() {
-    if (activebar === "bar1") {
-        if (progress1 < 100) {
-            progress1 += 0.5; // Adjust speed here
-            jobProgress1.style.width = progress1 + "%";
+    update() {
+        if (this.progress < this.maxprogress) {
+            this.progress += this.speed;
+            let widthPercent = Math.min((this.progress / this.maxprogress) * 100, 100);
+            this.element.style.width = widthPercent + "%";
         } else {
-            progress1 = 0;
-            if  (progress1>100) {
-                str = str + 1
-            }
+            this.level++;
+            this.progress = 0;
+            this.maxprogress = this.maxprogress*1.2
         }
     }
 
-    if (activebar === "bar2") {
-        
-        if (progress2 < 100) {
-            progress2 += 2; // Adjust speed here
-            jobProgress2.style.width = progress2 + "%";
-        } else {
-            progress2 = 0;
-            if (progress2>100) {
-                str = str + 1
-            }
-        }
+    reset() {
+        this.progress = 0;
+        this.level = 1;
+        this.element.style.width = "0%";
+
+    }
+
+    getLevel() {
+        return this.level;
     }
 }
 
+const relaxBar = new ProgressBar("jobrelax", 10, 100);
+const focusBar = new ProgressBar("jobfocus", 10, 100);
+
+
+let activeBar = null;
+
+function updateProgress() {
+    if (activeBar === "relax") {
+        relaxBar.update();
+        document.getElementById("relaxLevelDisplay").innerText = "Relax Level: " + relaxBar.getLevel();
+    } else if (activeBar === "focus") {
+        focusBar.update();
+        document.getElementById("focusLevelDisplay").innerText = "Focus Level: " + focusBar.getLevel();
+    }
+}
+
+
+
 setInterval(updateProgress, 100);
 
-const button1 = document.getElementById("button1")
-button1.addEventListener("click", () => {
-    activebar = "bar1";
+
+
+
+document.getElementById("button1").addEventListener("click", () => {
+    activeBar = "relax";
 });
 
-const button2 = document.getElementById("button2")
-button2.addEventListener("click", () => {
-    activebar = "bar2";
+document.getElementById("button2").addEventListener("click", () => {
+    activeBar = "focus";
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -57,5 +86,9 @@ button2.addEventListener("click", () => {
 /*
 V1 - Created this, and added elements.
 V1.1 - Added buttons and bars that fill up (extremely hard to add)
+V1.1.1 - Changed styles of bar and changed text/varibles
 
+V1.2 - Switched to OOP and cleaned up code
+V1.3 - Added level counters, and increasing difficulty
+V1.4 - Fixed a visual bug with some math
 */
